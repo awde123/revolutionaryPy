78cf9c9dcad4f79e48c992953dde82b74f174120
Gregory Thomas Croisdale <gtcroisdale1@gmail.com>
2017-03-28 20:31:22 -0400

    It works

--- genTable.py
+++ genTable.py
@@ -1,11 +1,34 @@
 import math
-print("sin(x) = sinx, cos(x) = cosx, tan(x) = tanx")
-f = input("f(x): ")
-g = input("g(x): ")
-ran = input("xMin, xMax: ").split(",")
-for x in range(int(ran[0]),int(ran[1]) + 1):
-    sinx = math.sin(x)
-    cosx = math.cos(x)
-    tanx = math.tan(x)
-    print(eval(f))
-print()
+from math import sin, cos, tan, exp, sqrt
+
+fVal = []
+gVal = []
+
+f = eval("lambda x: {0}".format(input("f(x): ")))
+g = eval("lambda x: {0}".format(input("g(x): ")))
+x = min = float(input("x min: "))
+max = float(input("x max: "))
+delx = float(input("delta x: "))
+delta = .000001
+
+while x <= max:
+    fVal += [f(x)]
+    gVal += [g(x)]
+    x += delx
+
+def cfunc(x):
+    return (f(x) - g(x))
+
+def cderiv(x):
+    return (cfunc(x+delta)-cfunc(x)) / delta
+
+def rt(guess=2, trials=12, func=cfunc, deriv=cderiv):
+    for i in range(0, trials):
+        guess = guess - (func(guess)/deriv(guess))
+    return guess
+
+print (rt(guess=min, trials=20))
+
+print (rt(guess=max, trials=20))
+print(fVal)
+print(gVal)
