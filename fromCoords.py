7347461a1888128e12fa1f923cad46b3a41226d0
Gregory Thomas Croisdale <gtcroisdale1@gmail.com>
2017-03-31 16:03:14 -0400

    fixed

--- fromCoords.py
+++ fromCoords.py
@@ -4,50 +4,59 @@
 import bpy, bmesh, os, math
 from mathutils import Vector, kdtree
 
-os.chdir("/Users/s97507/plot/revolutionaryPy/")
-
-bpy.data.objects['Function'].location = (0, 0, 0)
-
-def absMax(list): ## determines the number with the most distance from zero
+## determines the number with the most distance from zero
+def absMax(list):
     return max([max(map(float, list)), abs(min(map(float, list)))])
 
-def newBMesh():
-    ## initial mesh generation code
-    bm = bmesh.new()
-    bm.from_mesh(bpy.context.object.data)
-    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
-
-def plot(x, y): ## plots vertices according to x and y arrays
-    bm = bmesh.new()
-    bm.from_mesh(bpy.context.object.data)
-    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
-    for i in range(0,len(x) - 1):
-        bm.verts.new([float(x[i]),0.0,float(y[i])])
-        bm.to_mesh(bpy.context.object.data)
-    ## exports meshdata to object
-    bpy.ops.object.mode_set(mode='EDIT', toggle=False)
-
-def listIn(directory): ## turns file into array
+## turns file into list
+def listIn(directory): 
     i = open(directory, "r")
     return i.read().split(",")
 
-def selectVert(): ## selects all vertices
+## selects all vertices
+def selectVert(): 
     for v in mesh.verts:
         v.select = True
+
+def plotFunction(name, x, y):
+    ## Create new mesh
+    me = bpy.data.meshes.new(name)
+    ob = bpy.data.objects.new(name, me)
+    me.update()
+    scn = bpy.context.scene
+    scn.objects.link(ob)
+    scn.objects.active = ob
+    ob.select = True
+    bpy.data.objects[name].location = (0, 0, 0)
+    
+    ## get file input
+    os.chdir("/Users/s97507/plot/revolutionaryPy/")
+    xVal = listIn(x)
+    yVal = listIn(y)
+    
+    ## plot data and generate mesh
+    bm = bmesh.new()
+    bm.from_mesh(bpy.context.object.data)
+    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+    for i in range(0,len(xVal) - 1):
+        bm.verts.new([float(xVal[i]),0.0,float(yVal[i])])
+        bm.to_mesh(bpy.context.object.data)
         
-size = len(bm.verts)
-kd = kdtree.KDTree(size)
+    bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+    i = 0
+    for v in bmesh.from_edit_mesh(bpy.context.object.data).verts:
+        v.select = True
+        if i % 2 == 1:
+            bpy.ops.mesh.edge_face_add()
+            bpy.ops.mesh.select_all(action = 'DESELECT')
+            v.select = True
+        i += 1
+    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
 
-obj = bpy.context.edit_object
-me = obj.data
-bm = bmesh.from_edit_mesh(me)
-
-plot(listIn('x'),listIn('f'))
-
-plot(listIn('x'),listIn('g'))
+plotFunction("f(x)", "x", "f")
+plotFunction("g(x)", "x", "g")
 
 y = listIn('f') + listIn('g')
-
 x = listIn('x')
 
 ## exports and sets origin
@@ -55,4 +64,4 @@ bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
 bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
 
 ## sets camera y value
-bpy.data.objects['Camera'].location.y = max([absMax(y), absMax(x)]) / math.sin(0.5)
+bpy.data.objects['Camera'].location.y = max([absMax(y), absMax(x)]) / math.sin(1/3)
