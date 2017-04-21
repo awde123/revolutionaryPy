c710dbc46bdd1dc8a06d28100612af7c2228a1dd
Gregory Thomas Croisdale <gtcroisdale1@gmail.com>
2017-04-21 12:01:31 -0400

    made everythin all pretty :)

--- fromCoords.py
+++ fromCoords.py
@@ -1,6 +1,7 @@
 ## Created by Gregory Croisdale March 2017
 ## CreatePT for Computer Science Principles
 
+## library importation
 import bpy, bmesh, os, math
 from mathutils import Vector, kdtree
 
@@ -8,18 +9,19 @@ from mathutils import Vector, kdtree
 def absMax(list):
     return max([max(map(float, list)), abs(min(map(float, list)))])
 
-## turns file into list
-def listIn(directory): 
-    i = open(directory, "r")
+## turns coordinate file into list
+def listIn(directory):
+    i = open(directory + ".coord", "r")
     return i.read().split(",")
 
 ## selects all vertices
-def selectVert(): 
+def selectVert():
     for v in mesh.verts:
         v.select = True
 
-def plotFunction(name, x, y):
-    ## Create new mesh
+## plots and fills function according to table output
+def plotFunction(name, x, y, edge):
+    ## Create new mesh in default location
     me = bpy.data.meshes.new(name)
     ob = bpy.data.objects.new(name, me)
     me.update()
@@ -28,12 +30,12 @@ def plotFunction(name, x, y):
     scn.objects.active = ob
     ob.select = True
     bpy.data.objects[name].location = (0, 0, 0)
-    
+
     ## get file input
     os.chdir("/Users/s97507/plot/revolutionaryPy/")
     xVal = listIn(x)
     yVal = listIn(y)
-    
+
     ## plot data and generate mesh
     bm = bmesh.new()
     bm.from_mesh(bpy.context.object.data)
@@ -41,21 +43,28 @@ def plotFunction(name, x, y):
     for i in range(0,len(xVal) - 1):
         bm.verts.new([float(xVal[i]),0.0,float(yVal[i])])
         bm.to_mesh(bpy.context.object.data)
-        
+
+    ## prepares for filling
     bpy.ops.object.mode_set(mode='EDIT', toggle=False)
     bmf = bmesh.from_edit_mesh(bpy.context.object.data).verts
-    for j in range(0,len(bmf) - 2):
-        bmf.ensure_lookup_table()
-        bmf[j].select = True
-        bmf[j + 1].select = True
-        bpy.ops.mesh.edge_face_add()
-        bpy.ops.mesh.select_all(action = 'DESELECT')
 
+    if edge:  ## creates edges between vertices
+        for j in range(0,len(bmf) - 2):
+            bmf.ensure_lookup_table()
+            bmf[j].select = True
+            bmf[j + 1].select = True
+            bpy.ops.mesh.edge_face_add()
+            bpy.ops.mesh.select_all(action = 'DESELECT')
+    else:  ## creates face between vertices
+        bpy.ops.mesh.select_all(action = 'SELECT')
+        bpy.ops.mesh.edge_face_add()
+
+    ## finalizes object
     bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
 
-plotFunction("f(x)", "x", "f")
-plotFunction("g(x)", "x", "g")
-plotFunction("intersect", "xi", "i")
+plotFunction("f(x)", "x", "f", True)
+plotFunction("g(x)", "x", "g", True)
+plotFunction("intersect", "xi", "i", False)
 
 y = listIn('f') + listIn('g')
 x = listIn('x')
@@ -64,5 +73,6 @@ x = listIn('x')
 bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
 bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
 
-## sets camera y value
-bpy.data.objects['Camera'].location.y = max([absMax(y), absMax(x)]) / math.sin(1/3)
+## creates and adjusts camera
+cY = max([absMax(y), absMax(x)]) / math.sin(1/3), 0.0)
+bpy.ops.object.camera_add(view_align=False, enter_editmode=False, location=(0.0, cY, 0.0), rotation=(270.0*0.01745329251,180.0*0.01745329251,0.0))
--- genTable.py
+++ genTable.py
@@ -1,38 +1,43 @@
-import math
+## library importation and constant defintion for user use
 from math import sin, cos, tan, exp, sqrt
-
 pi = 3.14159265359
 e = 2.71828182846
 
+## initial variable declaration
 fVal = []
 gVal = []
 xVal = []
 iVal = []
 xiVal = []
 
+## user inputs python code to be used as function
 f = eval("lambda x: {0}".format(input("f(x): ")))
 g = eval("lambda x: {0}".format(input("g(x): ")))
+## user defines bounds and delta
 x = min = float(input("x min: "))
 max = float(input("x max: "))
 delx = float(input("delta x: "))
+## delta for derivative calculation
 delta = .000001
 
+## Newton's method:
+## function which subtracts user inputted equations for newton's method
 def cfunc(x):
     return (f(x) - g(x))
-
+## finds derivative with previously defined delta
 def cderiv(x):
     return (cfunc(x+delta)-cfunc(x)) / delta
-
+## newton's method, finds intersect of two equations
 def rt(guess=2, trials=12, func=cfunc, deriv=cderiv):
     for i in range(0, trials):
         guess = guess - (func(guess)/deriv(guess))
     return guess
 
+## defines two intersects
 int1 = rt(guess=min, trials=20)
 int2 = rt(guess=max, trials=20)
 
-print("intersection computation complete")
-
+## finds y values when functions are not intersecting yet
 while x <= max:
    if x <= int1 or x >= int2:
        fVal += [f(x)]
@@ -40,8 +45,7 @@ while x <= max:
        xVal += [x]
    x += delx
 
-print("non-intersection complete")
-
+## finds y values when functions are intersecting (for solid)
 x = int1
 while x < int2:
     iVal += [f(x)]
@@ -50,18 +54,15 @@ while x < int2:
     xiVal += [x]
     x += delx
 
-print("intersection complete")
-
+## adds final coordinate to f and g lists
 fVal += [f(int1), f(int2)]
 gVal += [g(int1), g(int2)]
 xVal += [int1, int2]
 
-print(fVal)
-print(gVal)
-print(xVal)
 
-open('f','w').write(str(fVal).replace("[","").replace("]",""))
-open('g','w').write(str(gVal).replace("[","").replace("]",""))
-open('x','w').write(str(xVal).replace("[","").replace("]",""))
-open('i','w').write(str(iVal).replace("[","").replace("]",""))
-open('xi','w').write(str(xiVal).replace("[","").replace("]",""))
+## exports data
+open('f.coord','w').write(str(fVal).replace("[","").replace("]",""))
+open('g.coord','w').write(str(gVal).replace("[","").replace("]",""))
+open('x.coord','w').write(str(xVal).replace("[","").replace("]",""))
+open('i.coord','w').write(str(iVal).replace("[","").replace("]",""))
+open('xi.coord','w').write(str(xiVal).replace("[","").replace("]",""))
